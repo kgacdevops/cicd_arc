4fc1adffeb8a9c65030ef443f57353d5d97fb574
Kevin Candole <kevin.candole@gmail.com>
2026-04-21 21:38:34 +0800

    removed tls setting

--- scripts/bash/setup_prereq_azure.sh
+++ scripts/bash/setup_prereq_azure.sh
@@ -28,7 +28,7 @@ echo "Creating Resource Group.."
 az group create --name "$resourceGroup" --location "$region"
 
 echo "Create Storage Account.."
-az storage account create --name "$storageAccount" --resource-group "$resourceGroup" --location "$region" --sku Standard_LRS --kind StorageV2 --min-tls-version TLS1_3 
+az storage account create --name "$storageAccount" --resource-group "$resourceGroup" --location "$region" --sku Standard_LRS --kind StorageV2
 
 echo "Create container on the Storage Account.."
 az storage container create --name "$storageContainer" --account-name "$storageAccount" --auth-mode login
